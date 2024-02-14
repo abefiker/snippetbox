29b4a56595342a45a596d4e26b8aec60d9460136
abfiker <new94abemelek@gmail.com>
2024-02-15 00:54:25 +0300

    "User authentication Routes setup "

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -56,7 +56,7 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	
+
 	data := app.newTemplateData(r)
 	data.Snippet = snippet
 
@@ -129,4 +129,20 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 		Expires: 365,
 	}
 	app.render(w, http.StatusOK, "create.tmpl.html", data)
+}
+
+func (app *application) userSignup(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Display a HTML form for signing up a new user...")
+}
+func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Create a new user...")
+}
+func (app *application) userLogin(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Display a HTML form for logging in a user...")
+}
+func (app *application) userLoginPost(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Authenticate and login the user...")
+}
+func (app *application) userLogoutPost(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Logout the user...")
 }
