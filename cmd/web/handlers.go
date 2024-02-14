074fa3ed5a5cf45f2b2c3e720d4b844470524c1e
abfiker <new94abemelek@gmail.com>
2024-02-14 22:20:25 +0300

    "back to working code"

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,20 +3,19 @@ package main
 import (
 	"errors"
 	"fmt"
+	"github.com/abefiker/snippetbox/internal/models"
+	"github.com/julienschmidt/httprouter"
 	"net/http"
 	"strconv"
-
-	"github.com/abefiker/snippetbox/internal/models"
-	"github.com/abefiker/snippetbox/internal/validator"
-	"github.com/julienschmidt/httprouter"
+	"strings"
+	"unicode/utf8"
 )
 
 type snippetCreateForm struct {
-	Title               string
-	Content             string
-	Expires             int
-	FieldErrors         map[string]string
-	validator.Validator `form:"-"`
+	Title       string
+	Content     string
+	Expires     int
+	FieldErrors map[string]string
 }
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
@@ -65,22 +64,50 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	var form snippetCreateForm
-	err := app.decodePostForm(r, &form)
+	err := r.ParseForm()
 	if err != nil {
 		app.clientError(w, http.StatusBadRequest)
 		return
 	}
-	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
-	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be more than 100 characters long")
-	form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
-	form.CheckField(validator.PermittedInt(form.Expires, 1, 7, 365), "expires", "This field must equal 1, 7 or 365")
-	if !form.Valid() {
+	// Get the expires value from the form as normal.
+	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+	if err != nil {
+		app.clientError(w, http.StatusBadRequest)
+		return
+	}
+	// Create an instance of the snippetCreateForm struct containing the values
+	// from the form and an empty map for any validation errors.
+	form := snippetCreateForm{
+		Title:       r.PostForm.Get("title"),
+		Content:     r.PostForm.Get("content"),
+		Expires:     expires,
+		FieldErrors: map[string]string{},
+	}
+	// Update the validation checks so that they operate on the snippetCreateForm
+	// instance.
+	if strings.TrimSpace(form.Title) == "" {
+		form.FieldErrors["title"] = "This field cannot be blank"
+	} else if utf8.RuneCountInString(form.Title) > 100 {
+		form.FieldErrors["title"] = "This field cannot be more than 100 characters long"
+	}
+	if strings.TrimSpace(form.Content) == "" {
+		form.FieldErrors["content"] = "This field cannot be blank"
+	}
+	if form.Expires != 1 && form.Expires != 7 && form.Expires != 365 {
+		form.FieldErrors["expires"] = "This field must equal 1, 7 or 365"
+	}
+	// If there are any validation errors re-display the create.tmpl template,
+	// passing in the snippetCreateForm instance as dynamic data in the Form
+	// field. Note that we use the HTTP status code 422 Unprocessable Entity
+	// when sending the response to indicate that there was a validation error.
+	if len(form.FieldErrors) > 0 {
 		data := app.newTemplateData(r)
 		data.Form = form
 		app.render(w, http.StatusUnprocessableEntity, "create.tmpl.html", data)
 		return
 	}
+	// We also need to update this line to pass the data from the
+	// snippetCreateForm instance to our Insert() method.
 	id, err := app.snippets.Insert(form.Title, form.Content, form.Expires)
 	if err != nil {
 		app.serverError(w, err)
@@ -91,6 +118,10 @@ func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	data := app.newTemplateData(r)
+	// Initialize a new createSnippetForm instance and pass it to the template.
+	// Notice how this is also a great opportunity to set any default or
+	// 'initial' values for the form --- here we set the initial value for the
+	// snippet expiry to 365 days.
 	data.Form = snippetCreateForm{
 		Expires: 365,
 	}
--- go.mod
+++ go.mod
@@ -8,4 +8,3 @@ require github.com/justinas/alice v1.2.0
 
 require github.com/julienschmidt/httprouter v1.3.0
 
-require github.com/go-playground/form/v4 v4.2.1 // indirect
